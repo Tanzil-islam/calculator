20aa35340353156259906b981f4330a04fd0f98e
Tanzil-islam <kazimdtanzilislam@gmail.com>
2022-05-21 15:48:18 +0600

    Calculator Program

---
+++ calculator  program.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() {
+    printf("\t\t ***********Welcome To My Calculator*******\n\n");
+
+    printf("Enter E to exit\n");
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+       case 'E':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
